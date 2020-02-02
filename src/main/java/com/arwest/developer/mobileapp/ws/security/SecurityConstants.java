dbcf9254a952f26f2fa1ba4bbce094ef4c1ed376
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-02-02 02:54:43 -0500

    fixing password reset url constant

--- src/main/java/com/arwest/developer/mobileapp/ws/security/SecurityConstants.java
+++ src/main/java/com/arwest/developer/mobileapp/ws/security/SecurityConstants.java
@@ -10,7 +10,7 @@ public class SecurityConstants {
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
     public static final String VERIFICATION_EMAIL_URL = "/users/email-verification";
-    public static final String PASSWORD_RESET_REQUEST_URL = "/password-reset-request";
+    public static final String PASSWORD_RESET_REQUEST_URL = "/users/password-reset-request";
     public static final String HEALTH = "/users/healthz";
 
 
