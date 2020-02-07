97c9eb4e8cb7177d96e9f5b6dffcaf690c747cc4
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-02-07 15:45:48 -0500

    initial test setup for JUnit 5

---
+++ src/test/java/com/arwest/developer/mobileapp/ws/service/impl/UserServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.arwest.developer.mobileapp.ws.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getUser() {
+    }
+}
